9c39e1a8cd0d6be3244864302367ea6d6289295c
fjonamalo <fjonamalo0@gmail.com>
2023-03-10 23:09:35 +0100

    Functions as Props (Delete Blog)

--- src/Home.js
+++ src/Home.js
@@ -7,9 +7,14 @@ const Home = () => {
     { title: "Welcome", body: "lorem ipsum...", author: "BB", id: 2 },
     { title: "Web dev top tips", body: "lorem ipsum...", author: "CC", id: 3 },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id == !id);
+    setBlogs(newBlogs);
+  };
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 };
