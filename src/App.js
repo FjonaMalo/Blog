aafe97c96aa46b124b5b7b2a5f874ce27582aabf
fjonamalo <fjonamalo0@gmail.com>
2023-03-11 13:13:35 +0100

    Using React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
