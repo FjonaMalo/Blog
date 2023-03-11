a219abdcfea95f6dbe4b65289bff92f086c54774
fjonamalo <fjonamalo0@gmail.com>
2023-03-11 11:38:25 +0100

    Using JSON Server

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,12 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("[UseEffect]");
+    console.log(blogs);
+  });
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
